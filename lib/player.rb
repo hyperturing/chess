b954a501f80b168cd516278bb0426a73f1b0bea6
Ash Peterson <ashnight07@gmail.com>
2020-03-01 16:25:55 -0600

    Player class added

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+class Player
+  attr_accessor :name
+
+  def initialize(name: 'Ashley')
+    @name = name
+  end
+
+  def move
+    puts "\n\n#{name}, please enter your move:"
+    move_notation = gets.chomp
+    move_notation
+  end
+end
